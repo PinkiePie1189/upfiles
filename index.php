0ba434ec4fbf471cd5fc23ef035c1f6aad816f79
PinkiePie1189 <mircea_costin84@yahoo.com>
2018-10-28 15:40:24 +0200

    First Prototype

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+</head>
 <form action="upload.php" method="post" enctype="multipart/form-data">
     <input type="file" name="file" id="file">
     <input type="submit" value="Upload File" name="submit">
